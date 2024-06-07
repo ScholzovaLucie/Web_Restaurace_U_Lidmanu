1cecf3c67a516736a835697f628dd646e5a7e4ec
ScholzovaLucie <lucka.scholz@gmail.com>
2024-06-07 17:14:08 +0200

    big update

---
+++ www/js/translations/translations_restaurace.js
@@ -0,0 +1,19 @@
+const translations = {
+  en: {
+    'text_upozorneni': '',
+    },
+  cz: {
+      'text_upozorneni': '',
+      },
+  pl: {
+    'text_upozorneni': '',
+    },
+  de: {
+    'text_upozorneni': '',
+    }
+};
+
+
+function changeLanguage(language) {
+  /*document.getElementById('text_upozorneni').innerHTML = translations[language].text_upozorneni;*/
+}
